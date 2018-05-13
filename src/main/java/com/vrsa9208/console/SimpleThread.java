f509ad7aaebbdae66b037aeaf49644f65da1688d
vrsa9208 <vrsa9208@gmail.com>
2018-05-13 16:24:22 -0500

    Add new Thread in SimpleThread example

--- src/main/java/com/vrsa9208/console/SimpleThread.java
+++ src/main/java/com/vrsa9208/console/SimpleThread.java
@@ -2,9 +2,19 @@ package com.vrsa9208.console;
 
 public class SimpleThread {
 	public static void main(String[] args) {
-		Runnable runnable = () -> System.out.println(Thread.currentThread().getName());
-		
-		Thread thread = new Thread(runnable);
-		thread.start();
+		Runnable runnable = () -> 
+			System.out.println("Thread name: " + Thread.currentThread().getName());
+
+		Thread thread0 = new Thread(runnable);
+		//I can change the name of the thread with the setName method
+		//thread1.setName("my-thread");
+		thread0.start();
+
+		Thread thread1 = new Thread(runnable);
+		thread1.start();
+
+		// If I use the run method on thread, it'll be executed in the main thread.
+		// Do not do this
+		thread1.run();
 	}
 }
