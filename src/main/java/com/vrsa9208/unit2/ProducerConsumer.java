df16314bea3dbb9bd48a0c481f6a11a253d9511d
vrsa9208 <vrsa9208@gmail.com>
2018-05-13 21:10:24 -0500

    Uses wait() and notify()

--- src/main/java/com/vrsa9208/unit2/ProducerConsumer.java
+++ src/main/java/com/vrsa9208/unit2/ProducerConsumer.java
@@ -1,7 +1,7 @@
 package com.vrsa9208.unit2;
 
 public class ProducerConsumer {
-	
+
 	private static Object lock = new Object();
 
 	private static int[] buffer;
@@ -10,59 +10,73 @@ public class ProducerConsumer {
 	static class Producer {
 		void produce() {
 			synchronized (lock) {
-				while (isFull(buffer)) { }
+				if (isFull(buffer)) {
+					try {
+						lock.wait();
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
 				buffer[count++] = 1;
+				lock.notify();
 			}
 		}
 	}
-	
+
 	static class Consumer {
 		void consume() {
 			synchronized (lock) {
-				while (isEmpty(buffer)) { }
+				if (isEmpty(buffer)) {
+					try {
+						lock.wait();
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
 				buffer[--count] = 0;
+				lock.notify();
 			}
 		}
 	}
-	
+
 	static boolean isEmpty(int[] buffer) {
 		return count == 0;
 	}
-	
+
 	static boolean isFull(int[] buffer) {
 		return count == buffer.length;
 	}
-	
+
 	public static void main(String[] args) throws InterruptedException {
 		buffer = new int[10];
 		count = 0;
-		
+
 		Producer producer = new Producer();
 		Consumer consumer = new Consumer();
-		
+
 		Runnable produceTask = () -> {
 			for (int i = 0; i < 50; i++) {
 				producer.produce();
 			}
 			System.out.println("Done producing");
 		};
-		
+
 		Runnable consumeTask = () -> {
 			for (int i = 0; i < 50; i++) {
 				consumer.consume();
 			}
 			System.out.println("Done consuming");
 		};
-		
+
 		Thread consumerThread = new Thread(consumeTask);
 		Thread producerThread = new Thread(produceTask);
-		
+
 		consumerThread.start();
 		producerThread.start();
-		
+
 		consumerThread.join();
 		producerThread.join();
-		
+
 		System.out.println("Data in the buffer: " + count);
 	}
 
